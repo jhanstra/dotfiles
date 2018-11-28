847a0af6712c16d9fa2712b7282ac9f19c99a4cf
Jared Hanstra <jhanstra@gmail.com>
2018-11-27 22:00:54 -0600

    rewrite linking script with node, add oclif

--- node-scripts/new-mac.js
+++ node-scripts/new-mac.js
@@ -1,41 +1,28 @@
 #!/usr/bin/env node
 
 const commander = require('commander')
+const path = require('path')
+const chalk = require('chalk')
+const fs = require('fs')
 const { exec } = require('child_process')
 
-const homedir = require('os').homedir()
+const home = require('os').homedir()
+const dotfiles = path.join(__dirname, '..')
 
-function vscode() {
-  process.chdir('/..')
-  const dotfiles = process.cwd()
-  console.log('cwd', dotfiles)
-  // const { stdout, stderr } = await exec(`rm "${home}/Library/Application Support/Code/User/settings.json"`)
-  // console.log(stdout)
-  // console.log(stderr)
+async function vscode() {
+  console.log(chalk.blue('Linking VSCode settings to dotfiles'))
+  await fs.unlink(`${home}/Library/Application Support/Code/User/settings.json`, () => {})
+  await fs.symlink(`${dotfiles}/vscode/settings.json`, `${home}/Library/Application Support/Code/User/settings.json`, function() {
+  })
+}
+
+async function karabiner() {
+  console.log(chalk.blue('Linking Karabiner settings to dotfiles'))
+  await fs.unlink(`${home}/.config/karabiner/karabiner.json`, function() {
+  })
+  await fs.symlink(`${dotfiles}/karabiner/karabiner.json`, `${home}/.config/karabiner/karabiner.json`, function() {
+  })
 }
 
 vscode()
-
-
-// ### this file is responsible for linking the vscode and karabiner settings, so they
-// ### can be hosted here but live in their correct locations
-
-// # set up - determine dotfiles root
-// cd "$(dirname "$0")/.."
-// DOTFILES_ROOT=$(pwd -P)
-
-// # remove any previous vscode settings and add a new file linked to our dotfiles
-// rm "$HOME/Library/Application Support/Code/User/settings.json"
-// ln -s "$DOTFILES_ROOT/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-
-
-// # remove any previous karabiner settings and add a new file linked to our dotfiles
-// rm "$HOME/.config/karabiner/karabiner.json"
-// ln -s "$DOTFILES_ROOT/karabiner/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
-
-
-// # cd into the npm-offline folder and run npm install to pre-install a shit ton of
-// # packages so we have them pre-cached. This allows us to npm install any of these
-// # packages even while offline.
-// cd npm-offline
-// npm install
+karabiner()
