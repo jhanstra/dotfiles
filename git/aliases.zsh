429d25d62556052bb7dcb9bb80a825d1139954db
jhanstra <jhanstra@indeed.com>
2019-12-11 10:48:00 -0600

    add git aliases and other little tweaks from Indeed computer

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,25 +7,36 @@ then
   alias git=$hub_path
 fi
 
+# statuses
 alias gs="git status -sb" # upgrade your git if -sb breaks for you. it's fun.
-alias gb="git branch"
-alias gba="git branch -a"
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s%Creset %Cgreen(%cr)%Creset%C(yellow)%d%Creset' --abbrev-commit --date=relative"
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s%Creset %Cgreen(%cr)%Creset%C(yellow)%d%Creset' --abbrev-commit --date=relative"
+alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r' # Remove `+` and `-` from start of diff lines; just rely upon color.
+
+# fetching and rebasing
 alias gf="git fetch"
-alias gc='git checkout'
 alias grom="git rebase origin/master"
 alias gfgrom="git fetch && git rebase origin/master"
-alias gp="git pull"
-alias ga="git add -A"
-alias gpom="git push origin master"
-alias git-delete-local-merged="git branch -d `git branch --merged | grep -v '^*' | grep -v 'master' | tr -d '\n'`"
-alias git-undo="git reset --soft HEAD^"
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r' # Remove `+` and `-` from start of diff lines; just rely upon color.
-alias rename='git branch -m'
-alias pull-latest='git checkout develop && git pull && git checkout - && git merge develop'
-alias amend='git commit --amend'
-alias adam='git add -A && git commit --amend'
 
-# ditto specfici
-alias gcd="git checkout develop"
+# adding and committing
+alias ga="git add -A"
+alias gac="git add -A && git commit"
+alias gacc="git add -A && git commit -m"
+alias gagc="git add -A && git commit"
+alias amend='git commit --amend -m'
+# gacp - check /functions dir
+
+# undoing
+alias gu="git reset --"
+
+# branches and checking out
+alias gb="git branch"
+alias gba="git branch -a"
+alias gcm="git checkout master"
+alias gnb="git checkout -b"
+alias gco="git checkout"
+alias gr='git branch -m'
+alias git-delete-local-merged="git branch -d `git branch --merged | grep -v '^*' | grep -v 'master' | tr -d '\n'`"
+
+# pushing and pulling
+alias gpom="git push origin master"
+alias gcmp="git checkout master && git pull"
