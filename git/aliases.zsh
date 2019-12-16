9ed385b9c71c746cc26d2399a93123e1fbf85627
jhanstra <jhanstra@indeed.com>
2019-12-16 10:27:18 -0600

    more git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,9 +19,9 @@ alias gfgrom="git fetch && git rebase origin/master"
 
 # adding and committing
 alias ga="git add -A"
-alias gac="git add -A && git commit"
-alias gacc="git add -A && git commit -m"
-alias gagc="git add -A && git commit"
+alias gc="git commit"
+alias gac="git add -A && git commit -m"
+alias gacc="git add -A && git commit"
 alias amend='git commit --amend -m'
 # gacp - check /functions dir
 
@@ -32,11 +32,14 @@ alias gu="git reset --"
 alias gb="git branch"
 alias gba="git branch -a"
 alias gcm="git checkout master"
+alias gcmp="git checkout master && git pull"
+alias gcb="git checkout -b"
 alias gnb="git checkout -b"
 alias gco="git checkout"
-alias gr='git branch -m'
+alias gbm='git branch -m'
+alias gbr='git branch -m'
 alias git-delete-local-merged="git branch -d `git branch --merged | grep -v '^*' | grep -v 'master' | tr -d '\n'`"
 
 # pushing and pulling
+alias gp="git pull"
 alias gpom="git push origin master"
-alias gcmp="git checkout master && git pull"
