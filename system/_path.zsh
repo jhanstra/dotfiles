a9a733b8a8063002954ef138820a2ad144e11276
jhanstra <jared@coprime.io>
2020-09-08 12:15:52 -0600

    add saveup and i commands

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,5 +1,5 @@
 
-export PATH="$ZSH/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/Users/jhanstra/Library/Android/sdk/platform-tools:/Users/jhanstra/env/bin:$PATH"
+export PATH="$ZSH/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/Users/jhanstra/Library/Android/sdk/platform-tools:/Users/jh/env/bin:/Users/jh/.deno/bin:/Applications/Postgres.app/Contents/Versions/12/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export ANDROID_HOME=~"/Library/Android/sdk"
 export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,6 +7,9 @@ alias latest="npm i @coprime/concept@latest @coprime/codash@latest @coprime/next
 alias dot='code ~/coprime/dotfiles'
 alias cdi='cd ~/indeed'
 alias reload="source ~/.zshrc"
+alias rl="source ~/.zshrc"
+alias vcp="vc --prod"
+alias prod="vc --prod"
 
 # node
 alias dev='npm run dev'
@@ -45,12 +48,27 @@ alias watchos="open /Applications/Xcode.app/Contents/Developer/Applications/Simu
 ## This is so much simpler
 
 dr() {
-  deno run --allow-write --allow-env --unstable $1
+  deno run -A --unstable $1
+}
+
+drw() {
+  denon run -A --unstable $1
 }
 
 dt() {
-  deno test --allow-write --allow-env --unstable $1
+  deno test --allow-read --allow-env --unstable $1
 }
+
+dtw() {
+  denon test -A --unstable tests.ts
+}
+
+e() {
+  # finds the project you want to open with 'z', then opens it in your editor
+  fasd_cd -d $1 && code .
+}
+
+
 
 di() {
   deno install
@@ -64,10 +82,43 @@ save() {
   git add -A && git commit -m $1
 }
 
+saveup() {
+  git add -A && git commit -m $1 && git push origin
+}
+
 bpatch() {
   npm run build && git add -A && git commit -m $1 && git push && npm version patch && npm publish
 }
 
+# i installs common sets of npm packages, e.g. all packages necessary for eslint, all packages necessary for a new Coprime project, etc
+i() {
+  if [ $1="eslint" ]
+  then
+    npm i -D @coprime/eslint-config babel-eslint eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-import-resolver-webpack eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier prettier-eslint eslint-plugin-mdx
+  elif [ $1="concept"]
+    npm i @coprime/concept
+  else
+    npm i $1
+  fi
+}
+
+
+ports() {
+  lsof -i tcp:$1
+}
+
+squash() {
+  git rebase -i HEAD~$1
+}
+
+v() {
+  if [ $# -eq 0 ]
+  then
+    vim .
+  else
+    fasd_cd -d $1 && vim .
+  fi
+}
 
 gacp() {
   git add -A && git commit -m $1 && git push
@@ -89,34 +140,7 @@ minor() {
   git add -A && git commit -m $1 && git push && npm version minor && npm publish
 }
 
-e() {
-  # finds the project you want to open with 'z', then opens it in your editor
-  fasd_cd -d $1 && code .
+
+patch() {
+  npm run build && git add -A && git commit -m $1 && git push && npm version patch && npm publish
 }
-
-# patch() {
-#   npm run build && git add -A && git commit -m $1 && git push && npm version patch && npm publish
-# }
-
-# ports() {
-#   lsof -i tcp:$1
-# }
-
-
-squash() {
-  git rebase -i HEAD~$1
-}
-
-v() {
-  if [ $# -eq 0 ]
-  then
-    vim .
-  else
-    fasd_cd -d $1 && vim .
-  fi
-}
-
-# get() {
-#   cd /Users/jaredhanstra/JH/offline/pages && wget -p -k -r --no-parent -U Mozilla "$1"
-
-# }
