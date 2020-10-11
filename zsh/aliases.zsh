c43f3b1fea71102339e71fa2bf27a16540378a6a
jhanstra <jared@coprime.io>
2020-10-10 20:31:55 -0500

    few changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,13 +15,11 @@ alias prod="vc --prod"
 alias dev='npm run dev'
 alias nd='npm run start:dev'
 alias tst='npm run test'
-alias start='npm run start'
+# alias start='npm run start'
 alias build='npm run build'
 alias lint="npm run lint"
 alias testAll="npm run test:all"
-alias deploy="now"
-alias deps="npm install react react-dom next styled-components"
-alias devDeps="npm install --save-dev @babel/core @babel/plugin-proposal-object-rest-spread @babel/preset-env babel-eslint babel-jest babel-loader babel-plugin-module-resolver babel-plugin-styled-components babel-register eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react prettier prettier-eslint"
+alias deploy="vercel"
 alias npmGlobal='npm ls -g -depth=0'
 alias bump='npm version patch --no-git-tag-version && npm publish'
 alias id='npm i && dev'
