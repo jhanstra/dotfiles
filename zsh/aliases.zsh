3c7846a838219ce2f40c6765dac213bffd285d5e
jhanstra <jared@coprime.io>
2021-09-23 12:35:08 -0600

    remove unneeded files and hide some from vscode

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -114,3 +114,7 @@ v() {
     fasd_cd -d $1 && vim .
   fi
 }
+
+## MacOS commands
+alias show-hidden-files='defaults write com.apple.finder AppleShowAllFiles YES'
+alias hide-hidden-files='defaults write com.apple.finder AppleShowAllFiles NO'
